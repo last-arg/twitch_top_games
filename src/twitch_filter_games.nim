44d9654bde437bf56fead1b8a32ebfbfdce6c9ae
last_arg <last_arg@tutanota.com>
2021-07-28 22:39:52 +0300

    fetch all twitch games

--- src/twitch_filter_games.nim
+++ src/twitch_filter_games.nim
@@ -1,4 +1,24 @@
-import asyncdispatch, httpclient, json, os, strformat, strutils
+import asyncdispatch, httpclient, json, os, strformat, strutils, options
+
+
+type
+  Game = ref object
+    id: string
+    name: string
+    box_art_url: string
+
+  Pagination = ref object
+    cursor: Option[string]
+
+  Games = ref object
+    data: seq[Game]
+    pagination: Pagination
+
+  GameTrimmed = ref object
+    id: string
+    name: string
+
+const twitch_client_id = "7v5r973dmjp0nd1g43b8hcocj2airz"
 
 proc getAccessToken(): Future[string] {.async.} =
   let client_secret = getEnv("TWITCH_CLIENT_SECRET")
@@ -23,7 +43,36 @@ proc getAccessToken(): Future[string] {.async.} =
     echo "Access token not found"
     return
 
+  client.close()
   return access_token
+
+proc fetchGames(token: string): Future[seq[Game]] {.async.} =
+  var games: seq[Game] = @[]
+  var client = newAsyncHttpClient()
+  client.headers = newHttpHeaders({
+    "Host": "api.twitch.tv",
+    "Connection": "close",
+    "Authorization": &"Bearer {token}",
+    "Accept": "application/vnd.twitchtv.v5+json",
+    "Client-Id": twitch_client_id
+  })
+  var cursor = ""
+  while true:
+    let url = &"https://api.twitch.tv/helix/games/top?first=100&after={cursor}"
+    let r = await client.get(url)
+    if not r.status.startsWith("200"):
+      echo &"Http request returned invalid status code: {r.status}"
+      return
+    let body = await r.body
+    let j = parseJson(body)
+    let g = to(j, Games)
+    games.add(g.data)
+    if g.pagination.cursor.isNone():
+      break
+    cursor = g.pagination.cursor.unsafeGet()
+
+  client.close()
+  return games
 
 proc asyncMain(): Future[void] {.async.} =
   const token_file = "tmp/twitch_access_token.txt"
@@ -41,7 +90,20 @@ proc asyncMain(): Future[void] {.async.} =
     f.close()
 
   echo &"Token: {access_token}"
-  discard
 
+  let games = await fetchGames(access_token)
+  echo &"Number of games: {games.len}"
+  let json_content = %*{"data": games}
+  let f = open("dist/top_games_list.json", fmReadWrite)
+  f.write($json_content)
+  f.close()
+  echo &"Wrote 'dist/top_games_list.json'"
+
+  let f_small = open("dist/top_games_list_small.json", fmReadWrite)
+  let games_small = cast[seq[GameTrimmed]](games)
+  let json_content_small = %*{"data": games_small}
+  f_small.write($json_content_small)
+  f_small.close()
+  echo &"Wrote 'dist/top_games_list_small.json'"
 
 waitFor asyncMain()
